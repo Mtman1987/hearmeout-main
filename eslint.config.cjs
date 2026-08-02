b7b5ba6fd9455d2178c1d7c376ac1d34cbf1814c
Mtman1987 <mtman1987@gmail.com>
2026-08-02 00:01:52 -0500

    Fix TypeScript lint configuration (#30)

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -3,7 +3,7 @@ const plugin = require('@typescript-eslint/eslint-plugin');
 
 module.exports = [
   {
-    files: ['**/*.{js,jsx,ts,tsx}'],
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
       parser,
       parserOptions: {
@@ -16,6 +16,28 @@ module.exports = [
     },
     rules: {
       'no-unused-vars': 'error',
+      'no-console': 'warn',
+    },
+  },
+  {
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      parser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': plugin,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      '@typescript-eslint/no-unused-vars': ['warn', {
+        argsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      }],
       'no-console': 'warn',
     },
   },
