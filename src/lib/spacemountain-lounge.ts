e13e9cec218c5000b78a0493f01c68d798cf685e
Mtman1987 <mtman1987@gmail.com>
2026-09-23 20:16:15 -0500

    Centralize Live HMO lounge authority constants

---
+++ src/lib/spacemountain-lounge.ts
@@ -0,0 +1,5 @@
+import { getRoomWatchSessionId } from '@/lib/watch-session';
+
+export const SPACEMOUNTAIN_LOUNGE_ROOM_ID = 'system-spacemountainlive-lounge';
+export const SPACEMOUNTAIN_LOUNGE_SESSION_ID = getRoomWatchSessionId(SPACEMOUNTAIN_LOUNGE_ROOM_ID, 'music');
+export const SPACEMOUNTAIN_LOUNGE_TWITCH_CHANNEL = 'mtman1987';
